73df34eeb4c05aba24269c9747bf638bdbe5538f
paulo reis <paulork07@gmail.com>
2022-06-24 15:29:17 -0300

    saved

--- src/template.ts
+++ src/template.ts
@@ -1,3 +1,6 @@
+import jszip from './static/jszip';
+import epubjs from './static/epubjs';
+
 export default `
 <!DOCTYPE html>
   <html>
@@ -5,9 +8,8 @@ export default `
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>EPUB.js</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/epubjs/dist/epub.min.js"></script>
-
+    <script type="text/javascript">${jszip}</script>
+    <script type="text/javascript">${epubjs}</script>
 
     <style type="text/css">
       body {
